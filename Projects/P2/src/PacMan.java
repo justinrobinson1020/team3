ec9e3ddf5b16e38ffa80cea20a64181af906b325
vgok <vgokaraju01@gmail.com>
2021-10-06 10:07:47 -0400

    wrote is_ghost_in_range method

--- Projects/P2/src/PacMan.java
+++ Projects/P2/src/PacMan.java
@@ -23,6 +23,14 @@ public class PacMan{
 	}
 
 	public boolean is_ghost_in_range() { 
+		if (myMap.getLoc(myLoc.shift(1, 0)) == Map.Type.Ghost || 
+			myMap.getLoc(myLoc.shift(0, 1)) == Map.Type.Ghost || 
+			myMap.getLoc(myLoc.shift(1, 1)) == Map.Type.Ghost ||
+			myMap.getLoc(myLoc.shift(-1, 0)) == Map.Type.Ghost ||
+			myMap.getLoc(myLoc.shift(0, -1)) == Map.Type.Ghost ||
+			myMap.getLoc(myLoc.shift(-1, -1)) == Map.Type.Ghost) {
+				return true;
+		}
 		return false;
 	}
 
