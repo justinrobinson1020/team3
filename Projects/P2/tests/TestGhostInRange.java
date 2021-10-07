0c986e04906398549e9b0c9f35d3c6c095a0ab0e
vgok <vgokaraju01@gmail.com>
2021-10-06 23:55:44 -0400

    Wrote tests for ghost's is_pacman_in_range method

--- Projects/P2/tests/TestGhostInRange.java
+++ Projects/P2/tests/TestGhostInRange.java
@@ -5,7 +5,18 @@ import java.io.*;
 public class TestGhostInRange extends TestCase {
 
 	public void testGhostInRange() throws FileNotFoundException{
-		return null;
+		Location pacmanloc = new Location(9, 11);
+		Location adj_ghostloc = new Location(9, 12);
+		Location nonadj_ghostloc = new Location(11, 12);
+		
+		PacMan pacman = frame.addPacMan(pacmanloc);
+		NoFrame frame = new NoFrame();
+
+		Ghost nonadj_ghost = frame.addGhost(nonadj_ghostloc, "Non-Adjacent Ghost", Color.red);
+		assertFalse(nonadj_ghost.is_pacman_in_range());
+
+		Ghost adj_ghost = frame.addGhost(adj_ghostloc, "Adjacent Ghost", Color.blue);
+		assertTrue(adj_ghost.is_ghost_in_range())
 	}
 }
 
