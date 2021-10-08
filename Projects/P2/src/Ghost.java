63fcb57dc9f88ee8853c4cd620fd3d325cc6221d
naaimaqadeer <53193964+naaimaqadeer@users.noreply.github.com>
2021-10-08 16:49:58 -0400

    Update Ghost.java

--- Projects/P2/src/Ghost.java
+++ Projects/P2/src/Ghost.java
@@ -26,8 +26,7 @@ public class Ghost{
 
 	public boolean attack() {
 		if (is_pacman_in_range() == true) {
-		    myMap.attack(myName);
-		    return true;
+		    return myMap.attack(myName)
 		}
 		return false;
 	}
