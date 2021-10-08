9e649bff4b6cac899f1282e3673aa6f83468f6b9
Shivani Amin <shivani.amin99@gmail.com>
2021-10-08 15:33:09 -0400

    Corrected functionality of getLoc method in Map.java

--- Projects/P2/src/Map.java
+++ Projects/P2/src/Map.java
@@ -60,13 +60,7 @@ public class Map{
 	
 	public HashSet<Type> getLoc(Location loc) {
 		//wallSet and emptySet will help you write this method
-		switch (field[loc]) {
-			case Map.Type.PACMAN: return Map.Type.PACMAN;
-			case Map.Type.GHOST: return Map.Type.GHOST;
-			case Map.Type.WALL: return wallSet;
-			case Map.Type.COOKIE: return Map.Type.COOKIE;
-			default: return emptySet;
-		}
+		return field.get(loc);
 	}
 
 	public boolean attack(String Name) {
