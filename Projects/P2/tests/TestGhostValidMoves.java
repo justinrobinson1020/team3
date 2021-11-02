643a0da2302c432815fc43e36d5688766aa48e77
alexlovescoding <75548162+alexlovescoding@users.noreply.github.com>
2021-11-02 15:30:33 -0400

    Update TestGhostValidMoves.java

--- Projects/P2/tests/TestGhostValidMoves.java
+++ Projects/P2/tests/TestGhostValidMoves.java
@@ -5,6 +5,15 @@ import java.io.*;
 public class TestGhostValidMoves extends TestCase {
 
 	public void testGhostValidMoves() throws FileNotFoundException{
-		return null;
+		MainFrame frame = new MainFrame(); 
+
+		// Creating Players
+		Ghost ghost = frame.addGhost(new Location(0, 0));
+		frame.getMap().add("Ghost", new Location(0,0),null,Type.GHOST);
+		
+		// Test that locations produced by get_valid_moves are actually movable. 
+		for(Location x : ghost.get_valid_moves()){
+			assertTrue(frame.getMap().move("Ghost",x,Type.GHOST));
+		}
 	}
 }
