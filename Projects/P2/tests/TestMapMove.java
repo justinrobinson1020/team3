bfd9ac8cc4d67aef3b39f4ab861bbebb85464368
alexlovescoding <75548162+alexlovescoding@users.noreply.github.com>
2021-11-02 15:24:54 -0400

    Update TestMapMove.java

--- Projects/P2/tests/TestMapMove.java
+++ Projects/P2/tests/TestMapMove.java
@@ -5,6 +5,16 @@ import java.io.*;
 public class TestMapMove extends TestCase{
 
 	public void testMapMove() {
-		return null;
+		// Creating Map
+		MainFrame frame = new MainFrame(); 
+
+		// Adding Pacman and Ghost
+		frame.getMap().add("Pacman", new Location(0,0),null,Type.PACMAN);
+		frame.getMap().add("Ghost", new Location(1,1),null,Type.GHOST);
+		frame.getMap().add("Wall", new Location(2,2),null,Type.WALL);
+
+		assertTrue(frame.getMap().move("Pacman",new Location(3, 3), Type.PACMAN));
+		assertTrue(frame.getMap().move("Ghost",new Location(2, 3), Type.GHOST));
+		assertFalse(frame.getMap().move("Wall",new Location(4, 3), Type.WALL));
 	}
 }
